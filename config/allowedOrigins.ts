eb53e86bd51c3091481d7dd924e82e52090877ba
itahTamar <tamar.verber@gmail.com>
2024-08-25 15:37:30 +0300

    set origins

--- config/allowedOrigins.ts
+++ config/allowedOrigins.ts
@@ -1,8 +1,9 @@
 export const allowedOrigins = [
-    // 'https://translate-game.onrender.com',
-    // 'https://www.tamarprojects.com',
+    'https://translate-game.onrender.com',
+    'https://www.tamarprojects.com',
     'http://localhost:5173',
+    'tamarprojects.com',
+    'https://tamarprojects.com',
     'test-front-vite.onrender.com',
-    'www.tamarprofile.site',
-    'tamarprojects.com'
+    'www.tamarprofile.site'
 ]
