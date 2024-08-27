1265ac32bea18d8a6027daaee927c1d9421d7957
itahTamar <tamar.verber@gmail.com>
2024-08-27 15:15:56 +0300

    add localhost origin

--- config/allowedOrigins.ts
+++ config/allowedOrigins.ts
@@ -2,6 +2,7 @@ export const allowedOrigins = [
     'https://translate-game.onrender.com',
     'https://www.tamarprojects.com',
     'http://localhost:5173',
+    'localhost:5173',
     'tamarprojects.com',
     'https://tamarprojects.com',
     'test-front-vite.onrender.com',
