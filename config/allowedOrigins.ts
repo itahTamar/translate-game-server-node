b29b743243414da511e153d80cf005611f503f3f
itahTamar <tamar.verber@gmail.com>
2024-08-29 14:08:26 +0300

    mute all no http origins

--- config/allowedOrigins.ts
+++ config/allowedOrigins.ts
@@ -1,16 +1,16 @@
 export const allowedOrigins = [
     'http://localhost:5173',
-    'localhost:5173',
+    // 'localhost:5173',
     
     'https://translate-game.onrender.com',
 
     'https://www.tamarprojects.com',
-    'tamarprojects.com',
+    // 'tamarprojects.com',
     'https://tamarprojects.com',
     
-    'test-front-vite.onrender.com',
-    'www.tamarprofile.site',
-    'tamarprofile.site',
+    // 'test-front-vite.onrender.com',
+    // 'www.tamarprofile.site',
+    // 'tamarprofile.site',
     'https://test-front-vite.onrender.com',
     'https://www.tamarprofile.site',
     'https://tamarprofile.site'
