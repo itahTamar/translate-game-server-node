2857b24778d3169ee151d8180d71ff7d1072606f
itahTamar <tamar.verber@gmail.com>
2025-02-23 13:30:48 +0200

    allowed origin fix

--- config/allowedOrigins.ts
+++ config/allowedOrigins.ts
@@ -1,2 +1,7 @@
-// export const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : [];
-export const allowedOrigins = ["http://localhost:5173"]
+export const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : [];
+// export const allowedOrigins = [
+//   "http://localhost:5173",
+//   "https://translate-game.onrender.com",
+//   "https://www.tamarprojects.com",
+//   "https://tamarprojects.com",
+// ];
