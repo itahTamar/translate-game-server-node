1f651f71ee57cf5e3accb2cddd9e770d76a8674d
itahTamar <tamar.verber@gmail.com>
2025-02-23 14:01:33 +0200

    add log to check CORS

--- config/corsOptions.ts
+++ config/corsOptions.ts
@@ -2,7 +2,7 @@ import {allowedOrigins} from './allowedOrigins'
 
 export const corsOptions = {
     origin: (origin, callback) => {
-        // console.log("allowed origin are:", allowedOrigins)
+        console.log("allowed origin are:", allowedOrigins)
         // console.log('Environment Variables:', process.env.ALLOWED_ORIGINS);
         if(allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
